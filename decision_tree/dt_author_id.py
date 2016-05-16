2a012db44c9bff1fb5518152eca2e078835f6e2c
Douglas Teoh <d.teoh@allm.net>
2016-05-16 16:43:11 +0900

    Finish Decision Tree mini-project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,12 +19,17 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+print "num_train_features={0}".format(len(features_train[0]))
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+print accuracy_score(labels_test, clf.predict(features_test))
 
 #########################################################
 
