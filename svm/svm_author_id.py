4b5e4b23a7e9941e111c3bc4c73bba6429d3f9ae
Douglas Teoh <d.teoh@allm.net>
2016-05-16 15:38:31 +0900

    Finish SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# Making the training set smaller
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+# Optimizing C for RBF kernel
+# for c in [10, 100, 1000, 10000]:
+for c in [10000]:
+    clf = SVC(C=c, kernel='rbf')
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+    print "C={0} accuracy={1}".format(c, accuracy_score(labels_test, pred))
+    # Extracting predictions
+    # for elem in [10, 26, 50]:
+    #     print "element={0} prediction={1}".format(elem, pred[elem])
+    print "prediction=1 num={0}".format(sum(pred))
 
 #########################################################
-
 
